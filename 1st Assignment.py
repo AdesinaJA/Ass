056fa5512030328e5a8a4dea70a1746879e90575
Adesina John <ayobamijohn12@gmail.com>
2020-01-17 09:51:53 -0500

    First commit

---
+++ 1st Assignment.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#S/N_1
+'''dot = ("*****************************\n" *4)
+print(dot)'''
+#S/N_5
+'''num = eval(input("Enter A Number: "))
+print("The square of the number is", num**2, sep=" ")'''
+#S/N_6
+'''num = eval(input("Enter a number: "))
+print(num*1, num*2, num*3, num*4, num*5, num*6, sep="---", end=".")'''
+#S/N_7
+'''weight=eval(input("Enter your weight in kilogram: "))
+print("Your weight in pounds is", weight*2.2, "lbs")'''
+#S/N_8
+'''num_1=eval(input("Enter Your 1st Number: "))
+num_2=eval(input("Enter Your 2nd Number: "))
+num_3=eval(input("Enter Your 3rd Number: "))
+print("total number=", num_1+num_2+num_3)
+print("average number=",(num_1+num_2+num_3)/3)'''
+#S/N_3
+'''print("*\n**\n***\n****\n*****")'''
+#S/N_2
+'''print("*****************\n*\t\t*\n*\t\t*\n*****************")'''
+#S/N_9
+ '''price=eval(input("Enter price of meal: "))
+tip=eval(input("Enter desired % tip: "))
+print("tip amount=", (tip/100)*price)
+print("total bill=", ((tip/100)*price)+price)'''
+#S/N_4
+'''num=eval(input("enter the calculation: "))
+print(num)'''
---
+++ 2nd Assignment.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Created on Sun Jan 12 18:51:38 2020"""
+name=input("Enter your name: ")
+matric_number=input("Enter Matric Number: ")
+session=input("Enter session: ")
+semester=input("Enter semester: ")
+level=input("Enter present level: ")
+program=input("Enter program: :")
+print(name,'\n',matric_number,'\n',session,'\n',semester,'\n',level,'\n',program)
+course_code="""
+BIO 101
+BIO103
+CHM101
+CHM191
+FAA101
+GNS101
+LIB101
+MTH101
+PHY101
+PHY103"""
+print(course_code)
+if course_code==0:
+    print(course_code==0)
+elif course_code+='A':
+    print(course_code==5)
+elif course_code+='B':
+    print(course_code==4)
